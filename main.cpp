e1a1e07dff82ea2a68c3a341d5a3f20abd5268c7
eszdman <eszdman2015@gmail.com>
2022-10-14 13:24:08 +0300

    Minor refactoring

--- main.cpp
+++ main.cpp
@@ -12,34 +12,35 @@ enum {
 enum {
     x1 = 0, x2, x3
 };
-
+typedef float fp_t;
 template<typename T>
 int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
 template<typename fp_t>
-int solve(vector<fp_t> &coefficients, vector<fp_t> &solution) {
-
-    solution.at(x3) =
-            coefficients.at(A) != 0 ? (abs(coefficients.at(B)) + abs(coefficients.at(C)) + abs(coefficients.at(D))) /
-                                      coefficients.at(A) : 0;
-    auto p = -coefficients.at(C) / 2;
-    auto q = sqrt(p * p - coefficients.at(B) * coefficients.at(D));
+int solve(vector<fp_t> &coeff, vector<fp_t> &roots) {
+    fp_t A = coeff[3];
+    fp_t B = coeff[2];
+    fp_t C = coeff[1];
+    fp_t D = coeff[0];
+    roots[x3] = A != 0 ? (abs(B) + abs(C) + abs(D))/A : 0;
+    auto p = coeff[C] / static_cast<fp_t>(-2L);
+    auto q = sqrt(p * p - B * D);
     if (q >= 0) {
         auto r = p + sgn(p) * q;
         if (r == 0) {
-            solution.at(x1) = coefficients.at(D) / coefficients.at(B);
-            solution.at(x2) = -x1;
+            roots[x1] = D / B;
+            roots[x2] = -x1;
             return 0;
         } else {
-            solution.at(x1) = coefficients.at(D) / r;
-            solution.at(x2) = r / coefficients.at(B);
+            roots[x1] = D / r;
+            roots[x2] = r / B;
             return 1;
         }
     } else {
-        solution.at(x1) = p / coefficients.at(B) + q / coefficients.at(B);
-        solution.at(x2) = p / coefficients.at(B) - q / coefficients.at(B);
+        roots[x1] = p / B + q / B;
+        roots[x2] = p / B - q / B;
         return 2;
     }
 
@@ -47,11 +48,23 @@ int solve(vector<fp_t> &coefficients, vector<fp_t> &solution) {
 }
 
 int main() {
-    vector<float> coefficients = {0, 2, -6, 1};
-    vector<float> solution = {.0, .0, .0};
-    cout << solve(coefficients, solution) << endl;
-    for (auto x: solution) {
-        cout << x << ' ';
+    int p = 3;
+    vector<fp_t> coefficients(p+1);
+    vector<fp_t> roots(p);
+    vector<fp_t> output(p);
+    generate_polynomial<fp_t>(p, 0, p, 0, 8.0L/5, -5, 5.0L, roots, coefficients);
+    fp_t dev;
+    cout << solve(coefficients, output) << endl;
+    int err = compare_roots<fp_t>(p,p,output,roots,dev);
+
+    cout<<"dev:"<<dev<<endl<<"roots:";
+    for (float root : roots) {
+        cout<<setprecision(numeric_limits<fp_t>::digits10 + 1)<< root << ' ';
     }
+    cout<<endl<<"output:";
+    for (float root : output) {
+        cout<<setprecision(numeric_limits<fp_t>::digits10 + 1)<< root << ' ';
+    }
+
     return 0;
 }
