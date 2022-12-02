5373a3ee916890130141592df4191d0859b99d32
eszdman <eszdman2015@gmail.com>
2022-12-02 13:58:00 +0300

    Upgrade accuracy

--- main.cpp
+++ main.cpp
@@ -17,10 +17,13 @@ enum {
 typedef float fp_t;
 
 complex<fp_t> fmaComplex(complex<fp_t> a, complex<fp_t> b, complex<fp_t> c) {
+
     return {pr_product_difference(a.real(), b.real(), a.imag(), b.imag()) + c.real(),
             fma(a.real(), b.imag(), fma(a.imag(), b.real(), c.imag()))};
 }
 
+#define SCF(x) static_cast<fp_t>(x)
+#define SCFC(x) static_cast<complex<fp_t>>(x)
 
 template<typename fp_t>
 void solve(vector<fp_t> &coefficients, vector<complex<fp_t>> &roots) {
@@ -33,7 +36,7 @@ void solve(vector<fp_t> &coefficients, vector<complex<fp_t>> &roots) {
 //        roots[x3] = (abs(B) + abs(C) + abs(D)) / A;
 //        roots[x3] = std::numeric_limits<fp_t>::epsilon(); // maybe framework bug
         roots[x3] = 0; // maybe framework bug
-        auto p = -C / static_cast<fp_t>(2.0L);
+        auto p = -C / SCF(2.0L);
         auto q = sqrt(pr_product_difference(p, p, B, D));
         if (std::numeric_limits<fp_t>::epsilon() > abs(q)) {
             auto r = fma(copysign(1, q), q, p);
@@ -54,30 +57,26 @@ void solve(vector<fp_t> &coefficients, vector<complex<fp_t>> &roots) {
             return;
         }
     } else {
-        auto b = -B / (A * static_cast<fp_t>(3.0L));
+        auto b = -B / (A * SCF(3.0L));
         auto c = C / A;
         auto d = D / A;
-//        auto s = fma(static_cast<fp_t>(3.0L) * b, b, -c);
-        auto s = pr_product_difference(static_cast<fp_t>(3.0L) * b, b, c, static_cast<fp_t>(1.0L));
-//        auto t = fma(fma(-b, b, s), b, -d);
-        auto t = pr_product_difference(pr_product_difference(static_cast<fp_t>(1.0L), s, b, b), b, d,
-                                       static_cast<fp_t>(1.0L));
-//        auto t = pr_product_difference(b, s, b, b * b) - d;
+        auto s = fma(SCF(3.0L) * b, b, -c);
+        //auto s = pr_product_difference(static_cast<fp_t>(3.0L) * b, b, c, static_cast<fp_t>(1.0L));
+        auto t = fma(fma(-b, b, s), b, -d);
+        //auto t = pr_product_difference(pr_product_difference(static_cast<fp_t>(1.0L), s, b, b), b, d,
+        //                               static_cast<fp_t>(1.0L));
+        //auto t = pr_product_difference(b, s, b, b * b) - d;
         complex<fp_t> y1, y2;
         if (s == 0) {
-            y1 = pow(-t, static_cast<fp_t>(1.0L / 3.0L));
-            y2 = y1 * static_cast<complex<fp_t>>((static_cast<complex<fp_t>>(1.iF) *
-                                                  static_cast<complex<fp_t>>(sqrt(static_cast<fp_t>(3.0L))) -
-                                                  static_cast<complex<fp_t>>(1.0L))
-                                                 / static_cast<complex<fp_t>>(2.0L));
+            y1 = pow(-t, SCF(1.0L / 3.0L));
             roots[x1] = b - y1;
-            roots[x2] = b - y2;
-            roots[x3] = y1 + y2 + b;
+            roots[x2] = fmaComplex(y1,-SCFC((SCFC(1.iF)*SCFC(sqrt(SCF(3.0L)))-SCFC(1.0L))/SCFC(2.0L)),b);
+            roots[x3] = y1 + fmaComplex(y1,SCFC((SCFC(1.iF)*SCFC(sqrt(SCF(3.0L)))-SCFC(1.0L))/SCFC(2.0L)),b);
             return;
         } else {
-            auto u = sqrt(static_cast<complex<fp_t>>(s / static_cast<fp_t>(3.0L) * static_cast<fp_t>(4.0L)));
-            auto v = asin(static_cast<complex<fp_t>>(static_cast<fp_t>(3.0L) * t) / (s * u)) / static_cast<fp_t>(3.0L);
-            auto w = (numbers::pi_v<fp_t> / static_cast<fp_t>(3.0L)) * copysign(static_cast<fp_t>(1.0L), v.real()) - v;
+            auto u = sqrt(SCFC(s / SCF(3.0L) * SCF(4.0L)));
+            auto v = asin(SCFC(SCF(3.0L) * t) / (s * u)) / SCF(3.0L);
+            auto w = copysign((numbers::pi_v<fp_t> / SCF(3.0L)), v.real()) - v;
             roots[x1] = fmaComplex(-sin(v), u, b);
             roots[x2] = fmaComplex(-sin(w), u, b);
             roots[x3] = roots[x1] + roots[x2] + b;
@@ -95,10 +94,10 @@ void solveReal(vector<fp_t> &coefficients, vector<fp_t> &roots) {
 
     if (std::numeric_limits<fp_t>::epsilon() > abs(A)) { // unused
         roots[x3] = (abs(B) + abs(C) + abs(D)) / A;
-        auto p = -C / static_cast<fp_t>(2.0L);
+        auto p = -C / SCF(2.0L);
         auto q = sqrt(pr_product_difference(p, p, B, D));
         if (std::numeric_limits<fp_t>::epsilon() > abs(q)) {
-            auto r = fma(copysign(1, q), q, p);
+            auto r = p + copysign(q,p*q);
             if (std::numeric_limits<fp_t>::epsilon() > abs(r)) {
                 roots[x1] = D / B;
                 roots[x2] = -roots[x1];
@@ -108,35 +107,30 @@ void solveReal(vector<fp_t> &coefficients, vector<fp_t> &roots) {
             }
         } else {
             roots[x1] = p + q / B;
-//            roots[x1] = fma(static_cast<fp_t>(1.0L) / B, q, p);
             roots[x2] = p - q / B;
-//            roots[x2] = fma(static_cast<fp_t>(1.0L) / B, -q, p);
         }
     } else {
-        auto b = -B / (A * static_cast<fp_t>(3.0L));
+        auto b = -B / (A * SCF(3.0L));
         auto c = C / A;
         auto d = D / A;
-        auto s = fma(static_cast<fp_t>(3.0L) * b, b, -c);
+        auto s = fma(SCF(3.0L) * b, b, -c);
         auto t = fma(fma(-b, b, s), b, -d);
         complex<fp_t> y1, y2;
         if (s == 0) {
-            y1 = pow(-t, static_cast<fp_t>(1.0L / 3.0L));
-            y2 = y1 * static_cast<complex<fp_t>>((static_cast<complex<fp_t>>(1.iF) *
-                                                  static_cast<complex<fp_t>>(
-                                                          sqrt(static_cast<fp_t>(3.0L))) -
-                                                  static_cast<complex<fp_t>>(1.0L))
-                                                 / static_cast<complex<fp_t>>(2.0L));
+            y1 = pow(-t, SCF(1.0L / 3.0L));
+            y2 = fmaComplex(y1,-SCFC((SCFC(1.iF)*SCFC(sqrt(SCF(3.0L)))-SCFC(1.0L))/SCFC(2.0L)),b);
             y1 = b - y1;
-            y2 = b - y2;
         } else {
-            auto u = sqrt(static_cast<complex<fp_t>>(s / static_cast<fp_t>(3.0L) * static_cast<fp_t>(4.0L)));
-            auto v = asin(static_cast<complex<fp_t>>(static_cast<fp_t>(3.0L) * t) / (s * u)) / static_cast<fp_t>(3.0L);
-            auto w = (numbers::pi_v<fp_t> / static_cast<fp_t>(3.0L)) * copysign(static_cast<fp_t>(1.0L), v.real()) - v;
+            auto u = sqrt(SCFC(s * SCF(4.0L/3.0L)));
+            auto v = asin(SCFC(SCF(3.0L) * t) / (s * u)) / SCF(3.0L);
+            auto w = copysign((numbers::pi_v<fp_t> / SCF(3.0L)), v.real()) - v;
             y1 = fmaComplex(-sin(v), u, b);
             y2 = fmaComplex(-sin(w), u, b);
         }
-        roots[x1] = copysign(hypot(y1.real(), y1.imag()), y1.real());
-        roots[x2] = copysign(hypot(y2.real(), y2.imag()), y2.real());
+        //roots[x1] = copysign(hypot(y1.real(), y1.imag()), y1.real());
+        //roots[x2] = copysign(hypot(y2.real(), y2.imag()), y2.real());
+        roots[x1] = copysign(((y1*complex<fp_t>(0,-y1.imag())).real()),y1.real());
+        roots[x2] = copysign(((y2*complex<fp_t>(0,-y2.imag())).real()),y2.real());
         roots[x3] = roots[x1] + roots[x2] + b;
     }
 }
@@ -145,7 +139,8 @@ template<typename fp_t>
 auto testPolynomial(unsigned int roots_count, vector<fp_t> &coeffs) {
     fp_t max_absolute_error, max_relative_error;
     vector<fp_t> roots(roots_count), coefficients(roots_count + 1);
-    generate_polynomial<fp_t>(roots_count, 0, roots_count, 0, MAX_DISTANCE, -1, 1, roots, coefficients);
+    generate_polynomial<fp_t>(roots_count, 0, roots_count, 0,
+                              MAX_DISTANCE, -1, 1, roots, coefficients);
     vector<fp_t> roots_computed(roots_count);
     if (roots_count + 1 < 4) {
         coefficients.resize(4);
@@ -186,7 +181,7 @@ int main() {
     cout << "Started" << endl;
 //    auto timer = Timer("compute");
 //    timer.start();
-    int roots = 2;
+    int roots = 3;
 
 #pragma omp parallel for
     for (auto i = 0; i < 1000'1000; ++i) {
